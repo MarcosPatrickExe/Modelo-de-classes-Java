515fdabd08ee03e4bb39c0ab6eeab8cbc73fedcc
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2020-07-06 23:55:38 -0300

    upload da classe 'cao.java'

---
+++ Interfaces/Cao.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public class Cao extends Mamifero implements AnimalDomesticado, AnimalEstimacao{
+
+	public void som() {};
+	public void andar() {};
+	
+    public void amamentar() {};
+	
+    public void alimentar() {};
+   	public void levarVeterinario() {};
+   	public void chamarVeterinario() {};
+   	
+   	public void brincar() {};
+	public void levarPassear() {};
+}
