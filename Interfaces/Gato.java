28af06a303e244a21f5c7eed261ee86a18853324
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2020-07-06 23:56:59 -0300

    upload da classe 'Gato.java'

---
+++ Interfaces/Gato.java
@@ -0,0 +1,17 @@
+package interfaces;
+
+public class Gato extends Mamifero implements AnimalDomesticado, AnimalEstimacao{
+     
+	public void som() {};
+	public void andar() {};
+	
+    public void amamentar() {};
+	
+    public void alimentar() {};
+   	public void levarVeterinario() {};
+   	public void chamarVeterinario() {};
+   	
+   	public void brincar() {};
+	public void levarPassear() {};
+	
+}
