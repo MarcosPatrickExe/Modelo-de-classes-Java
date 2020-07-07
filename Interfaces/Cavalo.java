520c7f88a8dc1deddcbaa6686fc0ab2b7ca386a6
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2020-07-06 23:56:17 -0300

    upload da classe 'Cavalo.java'

---
+++ Interfaces/Cavalo.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public class Cavalo extends Mamifero implements AnimalDomesticado{
+	public void som() {};
+	public void andar() {};
+	
+    public void amamentar() {};
+    
+    public void alimentar() {};
+	public void levarVeterinario() {};
+	public void chamarVeterinario() {};
+	
+	public void brincar() {};
+	public void levarPassear() {};
+}
