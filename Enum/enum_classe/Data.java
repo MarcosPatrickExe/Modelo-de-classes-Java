3f2fb5c283d0c9cafb73c184b7b625f522a98219
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2020-09-18 11:50:33 -0300

    upload do arquivo "Data.java"

---
+++ Enum/enum_classe/Data.java
@@ -0,0 +1,15 @@
+package enum_classe;
+
+public class Data {
+     private int dia;
+     private int mes;
+     private int ano;
+     private DiaSemana diaEnum;
+     
+     Data(int dia, int mes, int ano, DiaSemana diaEnum){
+    	 this.dia = dia;
+    	 this.mes = mes;
+    	 this.ano = ano;
+    	 this.diaEnum = diaEnum;
+     }
+}
