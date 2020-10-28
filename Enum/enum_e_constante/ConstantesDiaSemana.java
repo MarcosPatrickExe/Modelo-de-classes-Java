1c1e94da6f7dd2add1a666cf21af603fb958ea2d
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2020-10-28 15:02:37 -0300

    upload do arquivo "constantesDiaSemana.java"
    Classe com variáveis estáticas e constantes

---
+++ Enum/enum_e_constante/ConstantesDiaSemana.java
@@ -0,0 +1,11 @@
+package enum_e_constante;
+
+public class ConstantesDiaSemana {
+     static final int SEGUNDA = 1;
+     static final int TERCA = 2;
+     static final int QUARTA = 3;
+     static final int QUINTA = 4;
+     static final int SEXTA = 5;
+     static final int SABADO = 6;
+     static final int DOMINGO = 7;
+}
